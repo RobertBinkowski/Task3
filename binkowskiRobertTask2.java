08df4bbb41c0b8ec1dadc7242862cacf9e76624c
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2022-03-30 13:32:09 +0100

    test one

--- binkowskiRobertTask2.java
+++ binkowskiRobertTask2.java
@@ -1,29 +1,33 @@
-package cm;
+// package cm;
 
-import org.junit.jupiter.api.Assertions;
+import cm.CarParkKind;
+import cm.Period;
+import cm.Rate;
+
+import jdk.jfr.Name;
+import org.junit.jupiter.api.Test;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
-import org.junit.jupiter.api.Test;
-
 
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class binkowskiRobertTest {
 
     @Test
-    public void test1(){
+    public void test1() {
 
         BigDecimal normalRate = new BigDecimal(5);
         BigDecimal reducedRate = new BigDecimal(2);
 
         ArrayList<Period> usualPeriods = new ArrayList<>();
-        usualPeriods.add(new Period(10,17));
+        usualPeriods.add(new Period(10, 17));
         ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(17,24));
+        reducedPeriods.add(new Period(17, 24));
 
-        assertDoesNotThrow(() ->
-                new Rate(CarParkKind.STAFF, normalRate, reducedRate, reducedPeriods, usualPeriods));
+        assertDoesNotThrow(() -> new Rate(CarParkKind.STAFF, normalRate, reducedRate, reducedPeriods, usualPeriods));
     }
 
     @Test
@@ -33,189 +37,165 @@ class binkowskiRobertTest {
         BigDecimal reducedRate = new BigDecimal(2);
 
         ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
+        reducedPeriods.add(new Period(1, 5));
+        reducedPeriods.add(new Period(5, 12));
         ArrayList<Period> usualPeriods = new ArrayList<>();
         usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
+        usualPeriods.add(new Period(17, 24));
 
         CarParkKind kind = CarParkKind.STAFF;
         Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
 
-        assertDoesNotThrow( () -> rate.calculate(new Period(1,2)));
+        assertDoesNotThrow(() -> rate.calculate(new Period(1, 2)));
 
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         BigDecimal normalRate = new BigDecimal(5);
         BigDecimal reducedRate = new BigDecimal(2);
 
         ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
+        reducedPeriods.add(new Period(1, 5));
+        reducedPeriods.add(new Period(5, 12));
         ArrayList<Period> usualPeriods = new ArrayList<>();
         usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
+        usualPeriods.add(new Period(17, 24));
 
         CarParkKind kind = CarParkKind.STAFF;
         Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
 
-        assertDoesNotThrow(() -> rate.calculate(new Period(12,18)));
+        assertDoesNotThrow(() -> rate.calculate(new Period(12, 18)));
     }
+
     @Test
-    public void test4(){
+    public void test4() {
         BigDecimal normalRate = new BigDecimal(5);
         BigDecimal reducedRate = new BigDecimal(2);
 
         ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
+        reducedPeriods.add(new Period(1, 5));
+        reducedPeriods.add(new Period(5, 12));
         ArrayList<Period> usualPeriods = new ArrayList<>();
         usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
+        usualPeriods.add(new Period(17, 24));
 
         CarParkKind kind = CarParkKind.STAFF;
         Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
 
-        assertDoesNotThrow(() -> rate.calculate(new Period(8,24)));
+        assertDoesNotThrow(() -> rate.calculate(new Period(8, 24)));
     }
+
     @Test
-    public void test5(){
+    public void test5() {
         BigDecimal normalRate = new BigDecimal(5);
         BigDecimal reducedRate = new BigDecimal(2);
 
         ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
+        reducedPeriods.add(new Period(1, 5));
+        reducedPeriods.add(new Period(5, 12));
         ArrayList<Period> usualPeriods = new ArrayList<>();
         usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
+        usualPeriods.add(new Period(17, 24));
 
         CarParkKind kind = CarParkKind.STAFF;
         Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
 
-        assertDoesNotThrow(() -> rate.calculate(new Period(1,2)));
+        assertDoesNotThrow(() -> rate.calculate(new Period(1, 2)));
     }
+
     @Test
-    public void test6(){
-        BigDecimal normalRate = new BigDecimal(5);
-        BigDecimal reducedRate = new BigDecimal(-1);
-
-        ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
-        ArrayList<Period> usualPeriods = new ArrayList<>();
-        usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
-
-        CarParkKind kind = CarParkKind.STAFF;
-        Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
-
-        assertThrows(IllegalArgumentException.class, () -> rate.calculate(new Period(1,2)));
-    }
-    @Test
-    public void test7(){
-        BigDecimal normalRate = new BigDecimal(-1);
-        BigDecimal reducedRate = new BigDecimal(2);
-
-        ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
-        ArrayList<Period> usualPeriods = new ArrayList<>();
-        usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
-
-        CarParkKind kind = CarParkKind.STAFF;
-        Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
-
-        assertThrows(IllegalArgumentException.class, () -> rate.calculate(new Period(1,2)));
-    }
-    @Test
-    public void test8(){
-
-        BigDecimal normalRate = new BigDecimal(0);
-        BigDecimal reducedRate = new BigDecimal(2);
-
-        ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
-        ArrayList<Period> usualPeriods = new ArrayList<>();
-        usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
-
-        CarParkKind kind = CarParkKind.STAFF;
-        Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
-
-        assertDoesNotThrow(() -> rate.calculate(new Period(1,24)));
-    }
-    @Test
-    public void test9(){
+    public void test6() {
         BigDecimal normalRate = new BigDecimal(5);
         BigDecimal reducedRate = new BigDecimal(0);
 
         ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
+        reducedPeriods.add(new Period(1, 5));
+        reducedPeriods.add(new Period(5, 12));
         ArrayList<Period> usualPeriods = new ArrayList<>();
         usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
+        usualPeriods.add(new Period(17, 24));
 
         CarParkKind kind = CarParkKind.STAFF;
         Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
 
-        assertDoesNotThrow(() -> rate.calculate(new Period(1,24)));
+        assertDoesNotThrow(() -> rate.calculate(new Period(1, 24)));
     }
+
     @Test
-    public void test10(){
+    public void test7() {
         BigDecimal normalRate = new BigDecimal(5);
         BigDecimal reducedRate = new BigDecimal(2);
 
         ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
+        reducedPeriods.add(new Period(1, 5));
+        reducedPeriods.add(new Period(5, 12));
         ArrayList<Period> usualPeriods = new ArrayList<>();
         usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
+        usualPeriods.add(new Period(17, 24));
 
         CarParkKind kind = CarParkKind.STAFF;
         Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
 
-        assertDoesNotThrow(() -> rate.calculate(new Period(1,24)));
+        assertDoesNotThrow(() -> rate.calculate(new Period(1, 24)));
     }
+
     @Test
-    public void test11(){
+    public void test8() {
         BigDecimal normalRate = new BigDecimal(5);
         BigDecimal reducedRate = new BigDecimal(2);
 
         ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
+        reducedPeriods.add(new Period(1, 5));
+        reducedPeriods.add(new Period(5, 12));
         ArrayList<Period> usualPeriods = new ArrayList<>();
         usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
+        usualPeriods.add(new Period(17, 24));
 
         CarParkKind kind = CarParkKind.STAFF;
         Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
 
-        assertThrows(IllegalArgumentException.class, () -> rate.calculate(new Period(18,-22)));
+        assertThrows(IllegalArgumentException.class, () -> rate.calculate(new Period(18, -22)));
     }
+
     @Test
-    public void test12(){
+    public void test9() {
         BigDecimal normalRate = new BigDecimal(5);
         BigDecimal reducedRate = new BigDecimal(2);
 
         ArrayList<Period> reducedPeriods = new ArrayList<>();
-        reducedPeriods.add(new Period(1,5));
-        reducedPeriods.add(new Period(5,12));
+        reducedPeriods.add(new Period(1, 5));
+        reducedPeriods.add(new Period(5, 12));
         ArrayList<Period> usualPeriods = new ArrayList<>();
         usualPeriods.add(new Period(12, 17));
-        usualPeriods.add(new Period(17,24));
+        usualPeriods.add(new Period(17, 24));
 
         CarParkKind kind = CarParkKind.STAFF;
         Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
 
-        assertThrows(IllegalArgumentException.class, () -> rate.calculate(new Period(-5,17)));
+        assertThrows(IllegalArgumentException.class, () -> rate.calculate(new Period(-5, 17)));
+    }
+
+    // Task 3 Section
+    @Test
+    @Name("Visitor Rate")
+    public void newTest1() {
+        BigDecimal normalRate = new BigDecimal(10);
+        BigDecimal reducedRate = new BigDecimal(10);
+
+        ArrayList<Period> reducedPeriods = new ArrayList<>();
+        reducedPeriods.add(new Period(1, 3));
+        reducedPeriods.add(new Period(4, 5));
+        ArrayList<Period> usualPeriods = new ArrayList<>();
+        usualPeriods.add(new Period(6, 12));
+        usualPeriods.add(new Period(17, 24));
+
+        CarParkKind kind = CarParkKind.VISITOR;
+        Rate rate = new Rate(kind, normalRate, reducedRate, reducedPeriods, usualPeriods);
+
+        Period period = new Period(6,7);
+
+        assertEquals(new BigDecimal(0), () -> rate.calculate(period));
     }
 
 }
